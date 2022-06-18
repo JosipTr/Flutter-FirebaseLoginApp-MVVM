7ff4eb3329a2053ea02d88531bf5b47a5354e63b
JosipTr <josiptroha@gmail.com>
2022-06-18 14:27:00 +0200

    Add FirebaseException to logIn method

--- lib/resources/firebase_auth_model.dart
+++ lib/resources/firebase_auth_model.dart
@@ -4,7 +4,12 @@ class FirebaseAuthModel {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future logIn(String email, String password) async {
+    try{
     await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
   }
 
   Stream<User?> stateChanges() {
