5a7c1ff69a3138ae2a446875c1014d5a16666862
JosipTr <josiptroha@gmail.com>
2022-06-18 12:51:48 +0200

    Create FirebaseAuthViewModel

--- lib/resources/firebase_auth_model.dart
+++ lib/resources/firebase_auth_model.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthModel {
-  final _firebaseAuthModel = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future logIn(String email, String password) async {
-    await _firebaseAuthModel.signInWithEmailAndPassword(email: email, password: password);
+    await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
   }
 
   Stream<User?> stateChanges() {
-    return _firebaseAuthModel.authStateChanges();
+    return _firebaseAuth.authStateChanges();
   }
 }
---
+++ lib/viewmodel/firebase_viewmodel.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_login_app_mvvm/resources/firebase_auth_model.dart';
+
+class FirebaseAuthViewModel {
+  final FirebaseAuthModel _firebaseAuthModel = FirebaseAuthModel();
+
+  Future logIn(String email, String password) async {
+    _firebaseAuthModel.logIn(email, password);
+  }
+
+  Stream<User?> stateChanges() {
+    return _firebaseAuthModel.stateChanges();
+  }
+}
